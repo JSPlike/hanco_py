dcce1e4cc0c1816c1f7f52655fa066baf8d10d00
JSPlike <moter2588@gmail.com>
2018-12-29 14:57:36 +0900

    commit 12.29

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 		url(r'^$', views.index, name="index"),
+		url(r'^join/$', views.signup, name="join"),
+		url(r'^login/$', views.signin, name="login"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'main/index.html', {})
 		
+
+def signup(request):
+
+	if request.method == "POST":
+		form = UserForm(request.POST)
+
+		if form.is_valid():
+			new_user = User.objects.create_user(**form.cleaned_data)
+			login(request, new_user)
+			return redirect('index')
+
+	else:
+		form = UserForm()
+		return render(request, 'user/add_user.html', {'form': form })
+
+
+
+def signin(request):
+
+	if request.method == "POST":
+		form = LoginForm(request.POST)
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			redirect(request, 'index')
+		else:
+			return HttpResponse('로그인 실패. 다시 시도 하세요')
+
+	else :
+		form = LoginForm()
+		return render(request, 'user/login.html', {'form': form})
+
+
+def logout(request):
+	logout(request)
