d4cdf10030be9fdf6ba39300625cbd7a4d3c4a77
JSPlike <moter2588@gmail.com>
2018-12-30 23:02:55 +0900

    joonyoung 12.30 index.html

--- hanco/settings.py
+++ hanco/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig', #main app 등록
+    'main.apps.MainConfig',  # main app 등록
 ]
 
 MIDDLEWARE = [
@@ -103,16 +103,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#STATICFILES_DIR = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
--- main/views.py
+++ main/views.py
@@ -5,44 +5,44 @@ from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 
+
 def index(request):
-	return render(request, 'main/index.html', {})
-		
+    return render(request, 'index.html', {})
+
 
 def signup(request):
 
-	if request.method == "POST":
-		form = UserForm(request.POST)
+    if request.method == "POST":
+        form = UserForm(request.POST)
 
-		if form.is_valid():
-			new_user = User.objects.create_user(**form.cleaned_data)
-			login(request, new_user)
-			return redirect('index')
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('index')
 
-	else:
-		form = UserForm()
-		return render(request, 'user/add_user.html', {'form': form })
-
+    else:
+        form = UserForm()
+        return render(request, 'user/add_user.html', {'form': form})
 
 
 def signin(request):
 
-	if request.method == "POST":
-		form = LoginForm(request.POST)
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
-		if user is not None:
-			login(request, user)
-			redirect(request, 'index')
-		else:
-			return HttpResponse('로그인 실패. 다시 시도 하세요')
+        if user is not None:
+            login(request, user)
+            redirect(request, 'index')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 하세요')
 
-	else :
-		form = LoginForm()
-		return render(request, 'user/login.html', {'form': form})
+    else:
+        form = LoginForm()
+        return render(request, 'user/login.html', {'form': form})
 
 
 def logout(request):
-	logout(request)
+    logout(request)
